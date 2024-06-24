ff200e77aa5436d496b9a04477fd23fb115f1450
qobilovv_o <160605882+ODILJON-QOBILOV@users.noreply.github.com>
2024-06-24 22:59:54 +0500

    Update main.py

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ from aiogram.contrib.fsm_storage.memory import MemoryStorage
 from aiogram.dispatcher import FSMContext
 import time
 
-API_TOKEN = '7015223960:AAHsWLBVwKrVxGpeAFRvwoV5enn85xklOmI'
+API_TOKEN = "#"
 bot = Bot(API_TOKEN)
 storage = MemoryStorage()
 dp = Dispatcher(bot, storage=storage)
@@ -14,16 +14,6 @@ dp = Dispatcher(bot, storage=storage)
 from app import database
 from app import buttons
 from app import states
-
-
-# def get_user_plans():
-    # user_id = message.from_user.id
-    # plans = database.get_plans(user_id)
-    # if plans:
-    #     text = ''
-    #     for i, plan in enumerate(plans):
-    #         text += f'{i+1}. {plan[2]}\n'
-
 
 @dp.message_handler(commands=['start'], state='*')
 async def send_welcome(message: types.Message):
@@ -74,15 +64,6 @@ async def check_my_money(message: types.Message):
 async def add_new_plan(message: types.Message):
     await message.reply('Write new plan', reply_markup=buttons.main_kb)
     await states.Add_Plan.plan.set()
-    # try:
-    #     @dp.message_handler()
-    #     async def add_plan(message: types.Message):
-    #         user_id = message.from_user.id
-    #         plan = message.text
-    #         database.add_plan(user_id, plan)
-    #         await message.reply('Plan added', reply_markup=buttons.main_kb)
-    # except:
-    #     await message.reply('Plan not added {Error}', reply_markup=buttons.main_kb)
 
 @dp.message_handler(state=states.Add_Plan.plan)
 async def add_plan_state(msg: types.Message, state: FSMContext):
@@ -167,129 +148,6 @@ async def edit_plan_pk(msg: types.Message, state: FSMContext):
 async def change_text_plan(message: types.Message, state: FSMContext):
     data = await state.get_data('id')
     await message.answer('Change ')
-
-
-
-
-
-# @dp.message_handler(text='Change text of plan')
-# async def change_text_plan(message: types.Message):
-#     await message.answer('Write plan number', reply_markup=types.ReplyKeyboardRemove())
-#     plans = database.get_plans(message.from_user.id)
-#     if len(plans) == 0:
-#         await message.reply('You have no plans', reply_markup=buttons.main_kb)
-#     else:
-#         text = ''
-#         for i, plan in enumerate(plans):
-#             if plan[-1] == 0:
-#                 text += f'{i+1}. {plan[2]} - ❌\n'
-#             else:
-#                 text += f'{i+1}. {plan[2]} - ✅\n'
-#         await message.answer(text, reply_markup=buttons.main_kb)
-#         await message.answer('Write plan number')
-#         await states.Edit_Plan.pk.set()
-
-
-# @dp.message_handler(state=states.Edit_Plan.pk)
-# async def edit_plan_pk(msg: types.Message, state: FSMContext):
-#     await state.update_data(pk=int(msg.text))
-#     id = int(msg.text)
-#     if database.check_plan_id(msg.from_id, id) is not None:
-#         pk = database.check_plan_id(msg.from_id, id)
-#         data = await state.update_data(pk=pk)
-#         await msg.answer('Write new plan')
-#         await states.Edit_Plan.edited_plan.set()
-#         # await msg.answer("Plan success edit", reply_markup=buttons.main_kb)
-#     else:
-#         await msg.answer("Plan not found", reply_markup=buttons.main_kb)
-
-# @dp.message_handler(state=states.Edit_Plan.edited_plan)
-# async def edit_plan_pk(msg: types.Message, state: FSMContext):
-#     await state.update_data(new_plan=msg.text)
-#     data = await state.get_data()
-#     pk = data.get('pk')
-#     # await state.update_data(new_plan=msg.text)
-#     data = await state.get_data()
-#     pk = data.get('pk')
-#     new_plan = msg.text
-#     database.edit_plan(msg.from_id, new_plan, pk)
-#     await msg.answer("Plan success edited", reply_markup=buttons.main_kb)
-#     await state.finish()
-
-
-# @dp.message_handler(text='Change status of plan')
-# async def change_status_plan(message: types.Message):
-#     plans = database.get_plans(message.from_user.id)
-#     if len(plans) == 0:
-#         await message.reply('You have no plans', reply_markup=buttons.main_kb)
-#     else:
-#         text = ''
-#         for i, plan in enumerate(plans):
-#             if plan[-1] == 0:
-#                 text += f'{i+1}. {plan[2]} - ❌\n'
-#             else:
-#                 text += f'{i+1}. {plan[2]} - ✅\n'
-#         await message.answer(text, reply_markup=buttons.main_kb)
-#         await message.answer('Write plan number')
-#         await states.Edit_Plan.pk.set()
-
-
-# @dp.message_handler(state=states.Edit_Plan.pk)
-
-    
-
-    
-# @dp.message_handler(text='Change plan')
-# async def edit_plan(message: types.Message):
-#     await message.answer('Write plan number', reply_markup=types.ReplyKeyboardRemove())
-#     await states.Edit_Plan.pk.set()
-
-# @dp.message_handler(state=states.Edit_Plan.pk)
-# async def edit_plan_pk(msg: types.Message, state: FSMContext):
-#     await state.update_data(pk=int(msg.text))
-#     id = int(msg.text)
-#     if database.check_plan_id(msg.from_id, id) is not None:
-#         pk = database.check_plan_id(msg.from_id, id)
-#         data = await state.update_data(pk=pk)
-#         await msg.answer('Write new plan')
-#         await states.Edit_Plan.edited_plan.set()
-#         # await msg.answer("Plan success edit", reply_markup=buttons.main_kb)
-#     else:
-#         await msg.answer("Plan not found", reply_markup=buttons.main_kb)
-
-
-# @dp.message_handler(state=states.Edit_Plan.edited_plan)
-# async def edit_plan_pk(msg: types.Message, state: FSMContext):
-#     await state.update_data(new_plan=msg.text)
-#     data = await state.get_data()
-#     pk = data.get('pk')
-#     # await state.update_data(new_plan=msg.text)
-#     data = await state.get_data()
-#     pk = data.get('pk')
-#     new_plan = msg.text
-#     database.edit_plan(msg.from_id, new_plan, pk)
-#     await msg.answer("Plan success edited", reply_markup=buttons.main_kb)
-#     await state.finish()
-
-# @dp.message_handler(text='Change status')
-# async def change_status(message: types.Message):
-#     await message.answer('Write plan number', reply_markup=types.ReplyKeyboardRemove())
-#     await states.Edit_Plan.pk.set()
-
-# @dp.message_handler(state=states.Edit_Plan.pk)
-# async def change_status_pk(msg: types.Message, state: FSMContext):
-#     await state.update_data(pk=int(msg.text))
-#     id = int(msg.text)
-#     if database.check_plan_id(msg.from_id, id) is not None:
-#         pk = database.check_plan_id(msg.from_id, id)
-#         data = await state.update_data(pk=pk)
-#         # await msg.answer(f'Your plan\'s status is {}', reply_markup=buttons.change_plan_status)
-#     else:
-#         await msg.answer("Plan not found", reply_markup=buttons.main_kb)
-
-# @dp.message_handler(text='Change plan status')
-
-
 
 
 if __name__ == '__main__':
